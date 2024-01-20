c96411dcb02d05585add0eaaf91c18928336b4d3
sdaheng <sdaheng@gmail.com>
2024-01-20 12:55:40 +0800

    add installation of vim-plug to setup.sh

--- setup.sh
+++ setup.sh
@@ -4,6 +4,10 @@
 git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
 
+# install vim-plug
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
 # setup my own init.vim
 if [ -d "~/.config/nvim" ]; then
     cp ./init.vim ~/.config/nvim
