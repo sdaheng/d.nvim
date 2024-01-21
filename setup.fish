3c489bf4f89b0678607713c2b56f13ce85ee8775
sdaheng <sdaheng@gmail.com>
2024-01-21 10:15:18 +0800

    add parameters to setup.sh

---
+++ setup.fish
@@ -0,0 +1,67 @@
+#!/bin/fish
+
+function install_awesome_vimrc
+    git clone --depth=1 https://github.com/amix/vimrc.git $HOME/.vim_runtime
+    sh $HOME/.vim_runtime/install_awesome_vimrc.sh
+end
+
+function install_vim_plug
+    sh -c 'curl -fLo "$HOME/.local/share/nvim/site/autoload/plug.vim" --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+end
+
+function update_awsome_vimrc
+    set current_path (pwd)
+
+    cd $HOME/.vim_runtime
+    git reset --hard
+    git clean -d --force
+    git pull --rebase
+    python3 update_plugins.py  # use python3 if python is unavailable
+
+    cd $current_path
+end
+
+function update_nvim_configs
+    if test -d "$HOME/.config/nvim"
+        cp ./init.vim $HOME/.config/nvim
+        cp -R ./lua $HOME/.config/nvim
+    else
+        echo "You need to install neovim first."
+    end
+end
+
+function help
+    echo "-i to install vimrc, vim-plug and configs. -u to update"
+end
+
+# first install vimrc
+# install vim-plug
+# setup my own init.vim
+
+if test (count $argv) -eq 0
+    help
+    exit 1
+end
+
+# 遍历所有参数
+for arg in $argv
+    switch $arg
+        case "-i"
+            install_awesome_vimrc
+            install_vim_plug
+            update_plugins
+            # 在这里处理 -u 参数的逻辑
+            break
+        case "-u"
+            update_awsome_vimrc
+            update_nvim_configs
+            break
+        case "*"
+            # 其他未知参数的处理
+            echo "Unknown option: $arg"
+            help
+            break
+    end
+end
+
--- setup.sh
+++ setup.sh
@@ -1,18 +1,61 @@
-#/bin/sh
+#!/bin/sh
+
+set -e
+
+function install_awesome_vimrc {
+    git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
+    sh ~/.vim_runtime/install_awesome_vimrc.sh
+}
+
+function install_vim_plug {
+    sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+}
+
+function update_awsome_vimrc {
+    current_path=$PWD
+
+    cd ~/.vim_runtime
+    git reset --hard
+    git clean -d --force
+    git pull --rebase
+    python3 update_plugins.py  # use python3 if python is unavailable
+
+    cd $current_path
+}
+
+function update_nvim_configs {
+    if [ -d "$HOME/.config/nvim" ]; then
+        cp ./init.vim ~/.config/nvim
+        cp -R ./lua ~/.config/nvim
+    else
+        echo "You need to install neovim first."
+    fi
+}
+
+function help {
+    echo "-i to install vimrc, vim-plug and configs. -u to update"
+}
 
 # first install vimrc
-git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
-sh ~/.vim_runtime/install_awesome_vimrc.sh
-
 # install vim-plug
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-
 # setup my own init.vim
-if [ -d "~/.config/nvim" ]; then
-    cp ./init.vim ~/.config/nvim
-    cp -R ./lua ~/.config/nvim
-else
-    echo "You need to install neovim first."
+
+if [ $# -eq 0 ]; then
+  help
+  exit 1
 fi
+
+# 遍历所有参数
+for arg in "$@"; do
+  if [ "$arg" = "-i" ]; then
+    install_awesome_vimrc
+    install_vim_plug
+    update_plugins
+    # 在这里处理 -u 参数的逻辑
+  elif [ "$arg" = "-u" ]; then
+    update_awsome_vimrc
+    update_nvim_configs
+  fi
+done
 
