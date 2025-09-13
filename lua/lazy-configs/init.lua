555fb5dd7da83cf7ebe45e26e0892cbd73433ee6
SDH <sdaheng@gmail.com>
2025-09-13 13:58:46 +0800

    add nvim-origami for folding

--- lua/lazy-configs/init.lua
+++ lua/lazy-configs/init.lua
@@ -193,7 +193,18 @@ require('lazy').setup({
       require('mason-configs')
     end
   },
+-- lazy.nvim
+{
+	"chrisgrieser/nvim-origami",
+	event = "VeryLazy",
+	opts = {}, -- needed even when using default config
 
+	-- recommended: disable vim's auto-folding
+	init = function()
+		vim.opt.foldlevel = 99
+		vim.opt.foldlevelstart = 99
+	end,
+},
   'mileszs/ack.vim',
   {
     "olimorris/codecompanion.nvim",
